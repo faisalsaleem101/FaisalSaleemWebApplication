3460208e28518f091479d5bb3d14e33bfbc0e733
Faisal Saleem <faisalsaleem101@hotmail.com>
2018-09-19 15:59:20 +0100

    Created unit test class for power point

---
+++ UnitTestProject/DocumentGenerator/PowerPoint/PowerPointTest.cs
@@ -0,0 +1,18 @@
+﻿using DocumentGenerator.PowerPoint;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace UnitTestProject.DocumentGenerator.PowerPoint
+{
+    public class PowerPointTest
+    {
+        [Fact]
+        public void CreateTable()
+        {
+            var tableGenerator = new TableGeneratorPP();
+            tableGenerator.Run();
+        }
+    }
+}
