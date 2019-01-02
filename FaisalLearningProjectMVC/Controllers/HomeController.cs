4c888b160d02c49309b3876c909bd88ebaf4dbdb
Faisal Saleem <faisalsaleem101@hotmail.com>
2019-01-02 20:44:28 +0000

    added sendMail method in the home controller to send mail

--- FaisalLearningProjectMVC/Controllers/HomeController.cs
+++ FaisalLearningProjectMVC/Controllers/HomeController.cs
@@ -1,13 +1,12 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Diagnostics;
+﻿using System.Diagnostics;
 using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using FaisalLearningProjectMVC.Models;
 using FaisalLearningProjectMVC.Data;
-using System.Data.SqlClient;
 using Microsoft.EntityFrameworkCore;
+using MimeKit;
+using MailKit.Net.Smtp;
 
 namespace FaisalLearningProjectMVC.Controllers
 {
@@ -36,11 +35,30 @@ namespace FaisalLearningProjectMVC.Controllers
             return View();
         }
 
-        public IActionResult Contact()
+        public async Task<IActionResult> Contact()
         {
             ViewData["Message"] = "Your contact page.";
 
+            await SendMail("Test");
+
             return View();
+        }
+
+        private async Task SendMail(string mailbody)
+        {
+            var message = new MimeMessage();
+            message.From.Add(new MailboxAddress("Faisal Saleem Web App", "faisalsaleem104@gmail.com"));
+            message.To.Add(new MailboxAddress("Faisal Saleem", "faisalsaleem101@hotmail.com"));
+            message.Subject = "Contact Us";
+            message.Body = new TextPart(MimeKit.Text.TextFormat.Html) { Text = "test2"};
+
+            using (var client = new SmtpClient())
+            {
+                client.Connect("smtp.gmail.com", 587, false);
+                client.Authenticate("faisalsaleem104@gmail.com", "Greencar3");
+                await client.SendAsync(message);
+                client.Disconnect(true);
+            }            
         }
 
         public IActionResult Error()
---
+++ FaisalLearningProjectMVC/Models/ContactFormModel.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace FaisalLearningProjectMVC.Models
+{
+    public class ContactFormModel
+    {
+        [Required]
+        public string Name { get; set; }
+        [Required]
+        public string LastName { get; set; }
+        [Required]
+        public string Email { get; set; }
+        [Required]
+        public string Message { get; set; }
+    }
+}
